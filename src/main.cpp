0c7a0156828023ce3ed54bcfb3ff721cf304a319
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-08-21 18:37:03 +0530

    added file argument with their validation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
 
-int main() {
+#include "huffman.hpp"
+#include "UIHelper.hpp"
 
-    std::cout << "hello world" << std::endl;
-    
-    return 0;
+
+int main(int argc, char* argv[]) {
+
+    std::string option = UIHelper::checkValidOptions(argc, argv);
+
+    std::filesystem::path input_file;
+    std::filesystem::path output_file;
+
+
+    huffman fileCompressor;
+
+    if(option == "--compress"){
+        input_file = fileManager::moveFileToDataDir(argv[2]);
+        output_file = fileManager::createFileinDataDir(input_file,".huff");
+
+        fileCompressor.compress(input_file, output_file);
+    } else if(option == "--decompress"){
+        input_file = fileManager::moveFileToDataDir(input_file);
+        output_file = fileManager::createFileinDataDir(input_file,".txt");
+
+        fileCompressor.decompress(input_file, output_file);
+    }
 }
