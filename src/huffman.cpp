5d863cb74d5a6554c91e48d6bf17013b75c281dc
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-08-22 23:12:17 +0530

    compress build FrequencyTable from buffer

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -2,6 +2,10 @@
 
 void  huffman::compress(const std::filesystem::path& decompressedFile,const std::filesystem::path& outputFile) {
     fileManager fileManager(outputFile, decompressedFile);
+
+    std::vector<char> buffer = fileManager.getbuffer();//build buffer
+
+    FreqTable.buildUsingBuffer(buffer);//build FrequencyTable
 }
 
 void huffman::decompress(const std::filesystem::path& compressedFile, const std::filesystem::path& outputFile) {
