9858f514a081c29e986247ecff9e2faf9a31a68e
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-08-27 14:45:14 +0530

    checkVersion of given file when use decompress

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,12 @@ int main(int argc, char* argv[]) {
     } else if(option == "--decompress"){
         fileManager::checkExtension(fs::path(argv[2]), ".huff");
         input_file = fileManager::moveFileToDataDir(argv[2]);
+        try{
+            fileManager::checkVersion(input_file, "HUFF 1.0.0");
+        } catch (const std::runtime_error& e) {
+            std::cerr << e.what() << std::endl;
+            return 1;
+        }
         output_file = fileManager::createFileinDataDir(input_file,".txt");
 
         fileCompressor.decompress(input_file, output_file);
